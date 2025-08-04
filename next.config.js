11f4ae17da0134b5c968582b4f8433594e8da17b
Marketa Polakova <polakovamarketa@email.cz>
2025-08-04 11:21:56 +0200

    next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
   i18n: {
     locales: ["cs", "en"], // seznam podporovaných jazyků
     defaultLocale: "cs", // výchozí jazyk (bez prefixu)
