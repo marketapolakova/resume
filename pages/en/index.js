d7b3a2d69c0feebd27fa966f028ce1ab4221c4e6
Marketa Polakova <polakovamarketa@email.cz>
2025-08-04 10:42:21 +0200

    fix path

--- pages/en/index.js
+++ pages/en/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
-import HomePage from "./homePage/HomePage";
+import HomePage from "../homePage/HomePage";
 
 function App() {
   const { locale } = useRouter();
