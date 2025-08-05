744467d2b90b344900fb6b0e22c1bab6878698b9
Marketa Polakova <polakovamarketa@email.cz>
2025-08-05 10:52:12 +0200

    change favicon

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,13 @@ export default class MyDocument extends Document {
     return (
       <Html lang="cs">
         <Head>
+          {/* favicon */}
+          <link rel="icon" href="/logo.png" />
+          {/* apple touch icon */}
+          <link rel="apple-touch-icon" href="/logo.png" />
+          {/* další globální link/meta, např. manifest */}
+          <link rel="manifest" href="/manifest.json" />
+          {/* viewport, SEO tagy, fonty… */}
           <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
         <body>
